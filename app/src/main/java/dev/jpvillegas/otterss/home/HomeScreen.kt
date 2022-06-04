3cd5633d7b0b38a84baf876d06209f05d55b2d8b
Juan Pablo Villegas <jppasa@gmail.com>
2022-06-03 21:53:07 -0600

    improve date. fix card style. add title

--- app/src/main/java/dev/jpvillegas/otterss/home/HomeScreen.kt
+++ app/src/main/java/dev/jpvillegas/otterss/home/HomeScreen.kt
@@ -2,8 +2,8 @@ package dev.jpvillegas.otterss.home
 
 import android.util.Log
 import androidx.compose.animation.*
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,13 +13,9 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -27,12 +23,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.rememberImagePainter
 import dev.jpvillegas.otterss.R
 import dev.jpvillegas.otterss.ui.theme.OtteRssTheme
+import dev.jpvillegas.otterss.util.DateUtils.contextualDate
+import dev.jpvillegas.otterss.util.DateUtils.parseDate
 import tw.ktrssreader.kotlin.model.item.AutoMixItem
 
 
@@ -62,6 +57,7 @@ fun HomeScreen() {
         enter = fadeIn() + slideInVertically(initialOffsetY = { it / 2 }),
         exit = fadeOut() + slideOutVertically { it / 2 }
     ) {
+        val context = LocalContext.current
         val listState = rememberLazyListState()
 
         LazyColumn(
@@ -70,36 +66,103 @@ fun HomeScreen() {
                 .background(MaterialTheme.colors.background)
                 .fillMaxSize()
         ) {
-            item { Spacer(modifier = Modifier.height(16.dp)) }
-            items(feedItems) { FeedItem(it) }
+            item {
+                Spacer(modifier = Modifier.height(32.dp))
+
+                Text(
+                    text = stringResource(id = R.string.articles),
+                    fontSize = 24.sp,
+                    fontFamily = FontFamily.Monospace,
+                    fontWeight = FontWeight.ExtraBold,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+
+            items(feedItems) {
+                FeedItem(it, onItemClick = { item ->
+                    Log.d("ITEMCLICK", "$item")
+                    //context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(item.link)))
+                })
+            }
         }
     }
 }
 
 @Composable
-fun FeedItem(item: AutoMixItem) {
+fun FeedItem(
+    item: AutoMixItem,
+    onItemClick: (AutoMixItem) -> Unit
+) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 16.dp, vertical = 8.dp)
+            .clickable { onItemClick(item) }
     ) {
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
             Text(
                 text = item.title ?: item.simpleTitle ?: "n/a",
-                fontWeight = FontWeight.Medium,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Monospace,
                 color = MaterialTheme.colors.onBackground,
                 textAlign = TextAlign.Start,
-                fontSize = 20.sp
+                fontSize = 18.sp
             )
 
             Spacer(modifier = Modifier.height(8.dp))
 
-            item.pubDate?.let {
-                Text(text = it)
+            item.pubDate.parseDate()?.let {
+                Text(
+                    text = contextualDate(
+                        date = it,
+                        todayStr = stringResource(id = R.string.today),
+                        yesterdayStr = stringResource(id = R.string.yesterday),
+                        daysAgoStr = stringResource(id = R.string.days_ago),
+                    ),
+                    fontSize = 12.sp,
+                    fontFamily = FontFamily.Monospace,
+                )
+            }
+
+            item.categories?.mapNotNull { it.name }?.let {
+                if (it.isNotEmpty()) {
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Row {
+                        Label(text = it.first())
+                        Spacer(modifier = Modifier.width(4.dp))
+                        if (it.size > 1) {
+                            Label(text = "+ ${it.size - 1}")
+                        }
+                    }
+                }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun Label(text: String) {
+    val backgroundColor = MaterialTheme.colors.secondary.copy(alpha = 0.5f)
+
+    Chip(
+        onClick = {},
+        colors = ChipDefaults.chipColors(
+            backgroundColor = backgroundColor,
+            contentColor = MaterialTheme.colors.onSecondary
+        ),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Text(
+            text = text,
+            fontSize = 12.sp,
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight.ExtraBold
+        )
     }
 }
 
--- app/src/main/java/dev/jpvillegas/otterss/util/DateUtils.kt
+++ app/src/main/java/dev/jpvillegas/otterss/util/DateUtils.kt
@@ -1,7 +1,9 @@
 package dev.jpvillegas.otterss.util
 
+import android.content.Context
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 object DateUtils {
     fun String?.parseDate(): Date? {
@@ -9,5 +11,40 @@ object DateUtils {
 
         return SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss", Locale.US)
             .parse(this)
+    }
+
+    fun contextualDate(
+        date: Date,
+        todayStr: String,
+        yesterdayStr: String,
+        daysAgoStr: String,
+    ): String {
+        val lastMidnightMillis = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }.timeInMillis
+
+        if (date.time > lastMidnightMillis) {
+            return todayStr
+        }
+
+        val diffMillis = lastMidnightMillis - date.time
+        val daysBetween = TimeUnit.DAYS.convert(diffMillis, TimeUnit.MILLISECONDS)
+
+        if (daysBetween < 2) {
+            return yesterdayStr
+        }
+
+        if (daysBetween < 7) {
+            return "$daysBetween $daysAgoStr"
+        }
+
+        if (daysBetween < 365) {
+            return SimpleDateFormat("EEEE MMM dd", Locale.US).format(date)
+        }
+
+        return SimpleDateFormat("EEEE MMM dd, yyyy", Locale.US).format(date)
     }
 }
